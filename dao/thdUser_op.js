756a1af3c439c59f171ff98837625da98a754d85
THD199145 <sherlock199145@hotmail.com>
2016-09-03 15:31:49 +0800

    Update thdUser_op.js

--- dao/thdUser_op.js
+++ dao/thdUser_op.js
@@ -1,4 +1,4 @@
-// 实现对thduser表的数据操作 2016-08-30
+// operate thduser table 2016-08-30
 var mysql=require('mysql');
 var conf=require('../config/dbconfig');
 var sql=require('./thdUser_SqlMapping');
@@ -8,7 +8,7 @@ var UtilTime=require('../Util/UtilTime');
 //var UserData={};
 //module.exports=UserData;
 
-//添加数据
+//insert
 exports.addUserData=function(username,pw,guid){
       var ary=[username,pw,1,UtilTime.getUtcTime(),UtilTime.getUtcTime(),UtilTime.getUtcTime()];
       return new Promise(function(resolve,reject){
@@ -22,7 +22,7 @@ exports.addUserData=function(username,pw,guid){
     })
   });
 }
-// 查找数据通过UserName
+// query by UserName and isactive=1
 exports.selectByUserName=function(username){
   return new Promise(function(resolve,reject){
     dbHelper.execQueryReadOnly(sql.selectByUserName,[username],function(err,rows){
