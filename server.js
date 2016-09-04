35fd31b2c483957d5393ea98a7d2be83779e2ac5
THD199145 <sherlock199145@hotmail.com>
2016-09-05 07:48:39 +0800

    reference cluster

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 function Run(main){
   var cluster=require('cluster');
   var numCPUs = require('os').cpus().length;
-  //引用
+  //reference
   if (cluster.isMaster) {
     console.log('[master] ' + "start master...");
 
     for (var i = 0; i < numCPUs; i++) {
       cluster.fork();
     }
-    //监听
+    //listening
     cluster.on('listening', function (worker, address) {
       console.log('[master] ' + 'listening: worker' + worker.id + ',pid:' + worker.process.pid + ', Address:' + address.address + ":" + address.port);
     });
-    //满血复活
+    //reset 
     cluster.on("death", function (worker) {
               worker = cluster.fork();
           });
